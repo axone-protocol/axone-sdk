8574af42f9384dac169c65b33d9e38612255720b
Benjamin DENEUX <benjamin.deneux@proton.me>
2024-08-19 12:00:42 +0200

    feat(auth): implemant auth handler

--- auth/jwt/http.go
+++ auth/jwt/http.go
@@ -3,6 +3,7 @@ package jwt
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -12,23 +13,27 @@ import (
 func (f *Factory) HTTPAuthHandler(proxy auth.Proxy) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		credential, err := io.ReadAll(request.Body)
-		if err != nil { //nolint:staticcheck,revive
-			// ...
+		if err != nil {
+			http.Error(writer, fmt.Errorf("failed to read request body credential: %w", err).Error(), http.StatusBadRequest)
+			return
 		}
 
 		id, err := proxy.Authenticate(context.Background(), credential)
-		if err != nil { //nolint:staticcheck,revive
-			// ...
+		if err != nil {
+			http.Error(writer, fmt.Errorf("failed to authenticate: %w", err).Error(), http.StatusUnauthorized)
+			return
 		}
 
 		token, err := f.IssueJWT(id)
-		if err != nil { //nolint:staticcheck,revive
-			// ...
+		if err != nil {
+			http.Error(writer, fmt.Errorf("failed to issue JWT: %w", err).Error(), http.StatusInternalServerError)
+			return
 		}
 
 		writer.Header().Set("Content-Type", "application/json")
-		if _, err := writer.Write([]byte(token)); err != nil { //nolint:staticcheck,revive
-			// ...
+		if _, err := writer.Write([]byte(token)); err != nil {
+			http.Error(writer, fmt.Errorf("failed to write response: %w", err).Error(), http.StatusInternalServerError)
+			return
 		}
 	})
 }
@@ -37,8 +42,7 @@ func (f *Factory) VerifyHTTPMiddleware(next auth.AuthenticatedHandler) http.Hand
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		id, err := f.VerifyHTTPRequest(request)
 		if err != nil {
-			writer.WriteHeader(http.StatusUnauthorized)
-			writer.Write([]byte(err.Error())) //nolint:errcheck
+			http.Error(writer, err.Error(), http.StatusUnauthorized)
 			return
 		}
 
