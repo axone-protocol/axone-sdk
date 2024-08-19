8d4df8f6e1f753ca146be2a71dcc5ebc79180b08
Benjamin DENEUX <benjamin.deneux@proton.me>
2024-08-19 16:10:58 +0200

    test(auth): add test for verify jwt token and middleware

--- auth/jwt/http_test.go
+++ auth/jwt/http_test.go
@@ -64,11 +64,85 @@ func TestFactory_HTTPAuthHandler(t *testing.T) {
 					Times(1)
 
 				Convey("When the HTTPAuthHandler is called", func() {
-					req, err := http.NewRequest("POST", "/", bytes.NewBuffer(test.body)) // nolint:noctx
+					req, err := http.NewRequest("POST", "/", bytes.NewBuffer(test.body)) //nolint:noctx
 					So(err, ShouldBeNil)
 
 					recorder := httptest.NewRecorder()
 					handler.ServeHTTP(recorder, req)
+
+					Convey("Then the response should match the expected status and body", func() {
+						So(recorder.Code, ShouldEqual, test.expectedStatus)
+						So(recorder.Body.String(), ShouldContainSubstring, test.expectedBodyContains)
+					})
+				})
+			})
+		})
+	}
+}
+
+func TestFactory_VerifyHTTPMiddleware(t *testing.T) {
+	// Generate a valid token for testing purpose
+	token, err := jwt.NewFactory([]byte("secret"), "issuer", 5*time.Second).IssueJWT(&auth.Identity{
+		DID:               "did:key:zQ3shpoUHzwcgdt2gxjqHHnJnNkBVd4uX3ZBhmPiM7J93yqCr",
+		AuthorizedActions: nil,
+	})
+	if err != nil {
+		t.Fatalf("failed to issue a test jwt token: %v", err)
+	}
+
+	tests := []struct {
+		name                 string
+		authHeader           string
+		expectedIdentity     *auth.Identity
+		expectedStatus       int
+		expectedBodyContains string
+	}{
+		{
+			name:       "valid token",
+			authHeader: fmt.Sprintf("Bearer %s", token),
+			expectedIdentity: &auth.Identity{
+				DID: "did:key:zQ3shpoUHzwcgdt2gxjqHHnJnNkBVd4uX3ZBhmPiM7J93yqCr",
+			},
+			expectedStatus:       200,
+			expectedBodyContains: "success",
+		},
+		{
+			name:                 "invalid token",
+			authHeader:           "Bearer invalid_token",
+			expectedStatus:       401,
+			expectedBodyContains: "token contains an invalid number of segments",
+		},
+		{
+			name:                 "missing token",
+			authHeader:           "",
+			expectedStatus:       401,
+			expectedBodyContains: "couldn't find bearer token",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			Convey("Given a JWT factory and mocked authenticated handler", t, func() {
+				factory := jwt.NewFactory([]byte("secret"), "issuer", 5*time.Second)
+
+				mockHandler := func(id *auth.Identity, w http.ResponseWriter, _ *http.Request) {
+					So(id, ShouldResemble, test.expectedIdentity)
+					w.WriteHeader(http.StatusOK)
+					_, err := w.Write([]byte("success"))
+					So(err, ShouldBeNil)
+				}
+
+				middleware := factory.VerifyHTTPMiddleware(mockHandler)
+
+				Convey("When the VerifyHTTPMiddleware is called", func() {
+					req, err := http.NewRequest("GET", "/", nil) //nolint:noctx
+					So(err, ShouldBeNil)
+					if test.authHeader != "" {
+						req.Header.Set("Authorization", test.authHeader)
+					}
+
+					recorder := httptest.NewRecorder()
+					middleware.ServeHTTP(recorder, req)
 
 					Convey("Then the response should match the expected status and body", func() {
 						So(recorder.Code, ShouldEqual, test.expectedStatus)
