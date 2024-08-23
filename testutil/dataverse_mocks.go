734054580a0b86b4d5872eba3729175b1d38c375
Benjamin DENEUX <benjamin.deneux@proton.me>
2024-08-23 09:27:53 +0200

    test(dataverse): generate mock for dataverse and cognitarium grpc client

--- testutil/dataverse_mocks.go
+++ testutil/dataverse_mocks.go
@@ -55,16 +55,16 @@ func (mr *MockClientMockRecorder) ExecGov(arg0, arg1, arg2 any) *gomock.Call {
 }
 
 // GetGovAddr mocks base method.
-func (m *MockClient) GetGovAddr(arg0 context.Context, arg1 string) (string, error) {
+func (m *MockClient) GetGovAddr(arg0 context.Context) (string, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetGovAddr", arg0, arg1)
+	ret := m.ctrl.Call(m, "GetGovAddr", arg0)
 	ret0, _ := ret[0].(string)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // GetGovAddr indicates an expected call of GetGovAddr.
-func (mr *MockClientMockRecorder) GetGovAddr(arg0, arg1 any) *gomock.Call {
+func (mr *MockClientMockRecorder) GetGovAddr(arg0 any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetGovAddr", reflect.TypeOf((*MockClient)(nil).GetGovAddr), arg0, arg1)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetGovAddr", reflect.TypeOf((*MockClient)(nil).GetGovAddr), arg0)
 }
