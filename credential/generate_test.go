766fcd27b843aa1a2627aa58e987090ea1f849b2
Benjamin DENEUX <benjamin.deneux@proton.me>
2024-09-05 16:25:45 +0200

    test(credential): add mocked descriptor for test generator

--- credential/generate_test.go
+++ credential/generate_test.go
@@ -1,76 +1,116 @@
+//nolint:lll
 package credential_test
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 
 	"github.com/axone-protocol/axone-sdk/credential"
-	"github.com/axone-protocol/axone-sdk/credential/template"
 	"github.com/axone-protocol/axone-sdk/testutil"
-	"github.com/hyperledger/aries-framework-go/pkg/doc/verifiable"
 	. "github.com/smartystreets/goconvey/convey"
 	"go.uber.org/mock/gomock"
 )
 
 func TestGenerator_Generate(t *testing.T) {
-	loader, _ := testutil.MockDocumentLoader()
-	controller := gomock.NewController(t)
+	t.Run("without parser", func(t *testing.T) {
+		Convey("Given a credential generator with mocked descriptor", t, func() {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
 
-	mockSigner := testutil.NewMockSigner(controller)
-	mockSigner.EXPECT().Sign(gomock.Any()).Return([]byte("signature"), nil).AnyTimes()
-	mockSigner.EXPECT().Alg().AnyTimes()
+			mockDescriptor := testutil.NewMockDescriptor(controller)
+			mockDescriptor.EXPECT().Generate().Return(nil, nil).Times(1)
+			generator := credential.New(mockDescriptor)
 
-	tests := []struct {
-		name      string
-		generator *credential.Generator
-		wantErr   error
-		check     func(*verifiable.Credential)
-	}{
-		{
-			name:      "without parser",
-			generator: credential.New(template.NewGovernance("datasetID", "addr")),
-			wantErr:   errors.New("no parser provided"),
-		},
-		{
-			name: "without signature",
-			generator: credential.New(template.NewGovernance("datasetID", "addr"),
-				credential.WithParser(credential.NewDefaultParser(loader))),
-			wantErr: nil,
-			check: func(vc *verifiable.Credential) {
-				So(len(vc.Proofs), ShouldEqual, 0)
-			},
-		},
-		{
-			name: "with signature",
-			generator: credential.New(template.NewGovernance("datasetID", "addr"),
-				credential.WithParser(credential.NewDefaultParser(loader)), credential.WithSigner(mockSigner, "did:example:123")),
-			wantErr: nil,
-			check: func(vc *verifiable.Credential) {
-				So(len(vc.Proofs), ShouldEqual, 1)
-				So(vc.Proofs[0]["verificationMethod"], ShouldEqual, "did:example:123")
-				So(vc.Proofs[0]["proofPurpose"], ShouldEqual, "authentication")
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			Convey("Given a credential generator", t, func() {
-				Convey("When generating a credential", func() {
-					vc, err := test.generator.Generate()
-
-					Convey("Then an error or vc should be returned", func() {
-						if test.wantErr == nil {
-							So(err, ShouldBeNil)
-							So(vc, ShouldNotBeNil)
-							test.check(vc)
-						} else {
-							So(err, ShouldNotBeNil)
-							So(err.Error(), ShouldEqual, test.wantErr.Error())
-						}
-					})
+			Convey("When generating a credential", func() {
+				vc, err := generator.Generate()
+				Convey("Then an error should be returned", func() {
+					So(err, ShouldNotBeNil)
+					So(err.Error(), ShouldEqual, "no parser provided")
+					So(vc, ShouldBeNil)
 				})
 			})
 		})
-	}
+	})
+
+	t.Run("error generate", func(t *testing.T) {
+		Convey("Given a credential generator with mocked descriptor", t, func() {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			mockDescriptor := testutil.NewMockDescriptor(controller)
+			mockDescriptor.EXPECT().Generate().Return(nil, errors.New("failed")).Times(1)
+			generator := credential.New(mockDescriptor)
+
+			Convey("When generating a credential", func() {
+				vc, err := generator.Generate()
+				Convey("Then an error should be returned", func() {
+					So(err, ShouldNotBeNil)
+					So(err.Error(), ShouldEqual, credential.NewVCError(credential.ErrGenerate, errors.New("failed")).Error())
+					So(vc, ShouldBeNil)
+				})
+			})
+		})
+	})
+
+	t.Run("with signer", func(t *testing.T) {
+		Convey("Given a credential generator with mocked descriptor", t, func() {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			buf := bytes.NewBufferString(`{"@context":["https://www.w3.org/2018/credentials/v1"],"type":["VerifiableCredential"],"credentialSubject":{"id":"did:example:123"}}`)
+			mockDescriptor := testutil.NewMockDescriptor(controller)
+			mockDescriptor.EXPECT().Generate().Return(buf, nil).Times(1)
+			mockDescriptor.EXPECT().IssuedAt().Times(1)
+			mockDescriptor.EXPECT().ProofPurpose().Return("proof").Times(1)
+
+			mockSigner := testutil.NewMockSigner(controller)
+			mockSigner.EXPECT().Sign(gomock.Any()).Return([]byte("signature"), nil).Times(1)
+			mockSigner.EXPECT().Alg().AnyTimes()
+
+			loader, _ := testutil.MockDocumentLoader()
+
+			generator := credential.New(mockDescriptor,
+				credential.WithParser(credential.NewDefaultParser(loader)),
+				credential.WithSigner(mockSigner, "did:example:123"),
+			)
+
+			Convey("When generating a credential", func() {
+				vc, err := generator.Generate()
+				Convey("Then VC should be returned with proofs", func() {
+					So(err, ShouldBeNil)
+					So(vc, ShouldNotBeNil)
+					So(len(vc.Proofs), ShouldEqual, 1)
+					So(vc.Proofs[0]["verificationMethod"], ShouldEqual, "did:example:123")
+					So(vc.Proofs[0]["proofPurpose"], ShouldEqual, "proof")
+				})
+			})
+		})
+	})
+
+	t.Run("without signer", func(t *testing.T) {
+		Convey("Given a credential generator with mocked descriptor", t, func() {
+			controller := gomock.NewController(t)
+			defer controller.Finish()
+
+			buf := bytes.NewBufferString(`{"@context":["https://www.w3.org/2018/credentials/v1"],"type":["VerifiableCredential"],"credentialSubject":{"id":"did:example:123"}}`)
+			mockDescriptor := testutil.NewMockDescriptor(controller)
+			mockDescriptor.EXPECT().Generate().Return(buf, nil).Times(1)
+
+			loader, _ := testutil.MockDocumentLoader()
+
+			generator := credential.New(mockDescriptor,
+				credential.WithParser(credential.NewDefaultParser(loader)),
+			)
+
+			Convey("When generating a credential", func() {
+				vc, err := generator.Generate()
+				Convey("Then VC should be returned without proofs", func() {
+					So(err, ShouldBeNil)
+					So(vc, ShouldNotBeNil)
+					So(len(vc.Proofs), ShouldEqual, 0)
+				})
+			})
+		})
+	})
 }
